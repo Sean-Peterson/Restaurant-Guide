20ae66ea129a5b3b6b0abf376c76e1d2f6b8914b
Sean Peterson and Zach Swanson <sean@>
2017-02-22 10:46:43 -0800

    add CRUD methods to cuisine class and pass tests

--- src/Cuisine.php
+++ src/Cuisine.php
@@ -4,7 +4,7 @@
             private $id;
             private $cuisine_name;
 
-            function __construct($id = null, $cuisine_name)
+            function __construct($cuisine_name, $id=null)
             {
                 $this->id = $id;
                 $this->cuisine_name = $cuisine_name;
@@ -22,6 +22,29 @@
               return $this->id;
             }
 
+            // need to write save, getall, deleteall to pass next tests.
+            static function deleteAll()
+            {
+                $GLOBALS['DB']->exec("DELETE FROM cuisine;");
+            }
+
+            function save()
+            {
+                $GLOBALS['DB']->exec("INSERT INTO cuisine (cuisine_name) VALUES ('{$this->cuisine_name}');");
+                $this->id = $GLOBALS['DB']->lastInsertId();
+            }
+
+            static function getAll()
+            {
+                $returned_cuisines = $GLOBALS['DB']->query("SELECT * FROM cuisine;");
+                $cuisines = array();
+                foreach ($returned_cuisines as $cuisine)
+                {
+                    $new_cuisine = new Cuisine($cuisine['cuisine_name'],$cuisine['id']);
+                    array_push($cuisines,$new_cuisine);
+                }
+                return $cuisines;
+            }
 
 
     }
--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -14,17 +14,17 @@ $DB = new PDO($server, $username, $password);
 
 class CuisineTest extends PHPUnit_Framework_TestCase
 {
-    // protected function tearDown()
-    // {
-    //     Cuisine::deleteAll();
-    // }
+    protected function tearDown()
+    {
+        Cuisine::deleteAll();
+    }
 
     function test_getters()
     {
         // Arrange
         $id = 1;
         $cuisine_name = 'Thai';
-        $test_cuisine = new Cuisine ($id, $cuisine_name);
+        $test_cuisine = new Cuisine ($cuisine_name,$id);
 
         // Act
         $result = array($test_cuisine->getId(), $test_cuisine->getCuisineName());
@@ -32,7 +32,37 @@ class CuisineTest extends PHPUnit_Framework_TestCase
 
         // Assert
         $this->assertEquals($result, $expected_result);
+    }
 
+    function test_save()
+    {
+      // Arrange
+      $cuisine_name = 'Thai';
+      $test_cuisine = new Cuisine ($cuisine_name);
+      $test_cuisine->save();
+
+      // Act
+      $result = Cuisine::getAll();
+
+      // Assert
+      $this->assertEquals($result[0],$test_cuisine);
+    }
+
+    function test_getAll()
+    {
+      // Arrange
+      $cuisine_name = 'Thai';
+      $cuisine_name2 = 'Mexican';
+      $test_cuisine = new Cuisine ($cuisine_name);
+      $test_cuisine->save();
+      $test_cuisine2 = new Cuisine ($cuisine_name2);
+      $test_cuisine2->save();
+
+      // Act
+      $result = Cuisine::getAll();
+
+      // Assert
+      $this->assertEquals($result,[$test_cuisine,$test_cuisine2]);
     }
 
 
